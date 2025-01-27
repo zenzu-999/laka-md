4d2abb036a2b9ef9912ec1766edd0be49e7b61b0
© ᴍʀ-ʟᴀᴋᴀ 🧸🖇️✨ <lakaofc@gmail.com>
2025-01-27 13:21:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-// Put Your Session Id 🐣 කියන එක කපලා අයින් කරලා ඔයාගේ Session id එක දාන්න.
+//👉 Put Your Session Id 🐣 👈කියන එක කපලා අයින් කරලා ඔයාගේ Session id එක දාන්න.
 SESSION_ID: process.env.SESSION_ID || "Put Your Session Id 🐣",
-// Put Your Mongode Url 🖇️ කියන එක කපලා අයින් කරලා ඔයාගේ Mongode Url එක දාන්න.
+//👉 Put Your Mongode Url 🖇️ 👈කියන එක කපලා අයින් කරලා ඔයාගේ Mongode Url එක දාන්න.
 MONGODB: process.env.MONGODB || "Put Your Mongode Url 🖇️",
 PREFIX: process.env.PREFIX || '.' ,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94760172100' : process.env.OWNER_NUMBER,
